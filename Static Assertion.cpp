2eb44b86726c0b60d78d4169d3d87dd5878cb4d5
Denis Zangarov <denisimo_98@yahoo.com>
2020-10-25 22:24:13 +0200

    Create Static Assertion.cpp

---
+++ Static Assertion.cpp
@@ -0,0 +1,18 @@
+#define _ASSERT_GLUE(a, b)  a ## b
+#define ASSERT_GLUE(a, b)   _ASSERT_GLUE(a, b)
+
+#define STATIC_ASSERT(expr) \
+    enum    \
+    {   \
+        ASSERT_GLUE(g_assert_fail_, __LINE__)   \
+            == 1 / (int) (!!(expr)) \
+    }
+
+
+struct NeedsToBe128Bytes
+{
+    U32 m_a;
+    F32 m_b;
+};
+
+STATIC_ASSERT(sizeof(NeedsToBe128Bytes)== 128, "wrong size");
