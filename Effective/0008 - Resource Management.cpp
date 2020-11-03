eb403413db660716f2572fa267beec7abcf0ea78
Denis Zangarov <denisimo_98@yahoo.com>
2020-11-03 13:48:08 +0200

    Update 0008 - Resource Management.cpp

--- Effective/0008 - Resource Management.cpp
+++ Effective/0008 - Resource Management.cpp
@@ -25,3 +25,71 @@ void f()
 } // automatically delete
 // pInv via shared_ptr’s dtor
 
+
+
+/*
+----------------------------Example 2----------------------------
+*/
+
+/*
+Not all resources are heap - based, however,
+and for such resources, smart pointers like auto_ptrand tr1::shared_ptr
+are generally inappropriate as resource handlers.That being the case,
+you’re likely to find yourself needing to create your own resourcemanaging
+classes from time to time.
+For example, suppose you’re using a C API to manipulate mutex
+objects of type Mutex offering functions lockand unlock:
+*/
+
+void lock(Mutex* pm); // lock mutex pointed to by pm
+void unlock(Mutex * pm); // unlock the mutex
+
+/*
+To make sure that you never forget to unlock a Mutex you’ve locked,
+you’d like to create a class to manage locks.The basic structure of
+such a class is dictated by the RAII principle that resources are
+acquired during constructionand released during destruction :
+*/
+
+
+class Lock {
+public:
+	explicit Lock(Mutex* pm)
+		: mutexPtr(pm)
+	{
+		lock(mutexPtr);
+	} // acquire resource
+	~Lock() { unlock(mutexPtr); } // release resource
+private:
+	Mutex* mutexPtr;
+};
+
+//Clients use Lock in the conventional RAII fashion :
+Mutex m; // define the mutex you need to use
+...
+{ // create block to define critical section
+	Lock ml(&m); // lock the mutex
+	... // perform critical section operations
+} // automatically unlock mutex at end of block
+This is fine, but what should happen if a Lock object is copied ?
+Lock ml1(&m); // lock m
+Lock ml2(ml1); // copy ml1 to ml2 — what should happen here?
+
+/*
+Fortunately, tr1::shared_ptr allows specification of a “deleter” — a
+function or function object to be called when the reference count
+goes to zero. (This functionality does not exist for auto_ptr, which
+always deletes its pointer.) The deleter is an optional second parameter
+to the tr1::shared_ptr constructor, so the code would look
+like this:
+*/
+class Lock {
+public:
+	explicit Lock(Mutex* pm) // init shared_ptr with the Mutex
+		: mutexPtr(pm, unlock) // to point to and the unlock func
+	{ // as the deleter†
+		lock(mutexPtr.get()); // see Item15 for info on “get”
+	}
+private:
+	std::tr1::shared_ptr<Mutex> mutexPtr; // use shared_ptr
+}; // instead of raw pointer
