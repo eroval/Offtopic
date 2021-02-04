30324888211d6b9ab5ee570c5b58a208ca48cf65
Denis Zangarov <denisimo_98@yahoo.com>
2021-02-04 15:47:12 +0200

    Create pytest015 - Module Ex.py

---
+++ Python Bootcamp/pytest015 - Module Ex.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+#relative path
+from module_ex.modulefile01 import myfunc
+
+from module_ex.package01 import module01
+from module_ex.package01 import module02
+
+myfunc()
+
+module01.countToTwenty()
+module02.countToTen()
