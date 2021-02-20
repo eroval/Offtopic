b216325d98497b06f7c34506d215689934954839
Denis Zangarov <denisimo_98@yahoo.com>
2021-02-20 03:06:35 +0200

    Update featureNormalize.m

--- Stanford AI/Octave/machine-learning-ex1/ex1/featureNormalize.m
+++ Stanford AI/Octave/machine-learning-ex1/ex1/featureNormalize.m
@@ -5,10 +5,16 @@ function [X_norm, mu, sigma] = featureNormalize(X)
 %   is 1. This is often a good preprocessing step to do when
 %   working with learning algorithms.
 
+%earlier solution
+%mu = [mean(X(:,1)) mean(X(:,2))];
+%sigma = [max(X(:,1))-min(X(:,1)) max(X(:,2))-min(X(:,2))];
+%X_norm=[(X(:,1)-mu(1))/sigma(1) (X(:,2)-mu(2))/sigma(2)]
+
 % You need to set these values correctly
-mu = [mean(X(:,1)) mean(X(:,2))];
-sigma = [max(X(:,1))-min(X(:,1)) max(X(:,2))-min(X(:,2))];
-X_norm=[(X(:,1)-mu(1))/sigma(1) (X(:,2)-mu(2))/sigma(2)]
+%later soulution
+mu = mean(X);
+sigma = std(X);
+X_norm = (X - mu)./sigma;
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: First, for each feature dimension, compute the mean
@@ -25,12 +31,6 @@ X_norm=[(X(:,1)-mu(1))/sigma(1) (X(:,2)-mu(2))/sigma(2)]
 %
 % Hint: You might find the 'mean' and 'std' functions useful.
 %       
-
-
-
-
-
-
 
 
 
